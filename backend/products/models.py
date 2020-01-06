47c3ab4de65c62dc4881834611372ee9bc420331
Leonel Mandarino <leonelmandarino@gmail.com>
2020-01-06 03:02:38 -0300

    Add Product model
    - Product model with many to many realtionship
    - Registered on admin site

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+# from django.urls import reverse
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200, blank=True)
+    quantity = models.FloatField()
+    price = models.FloatField(null=True, blank=True)
+
+    composition = models.ManyToManyField(
+        'self', symmetrical=False, related_name='compose', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('product', args=[str(self.pk)])
